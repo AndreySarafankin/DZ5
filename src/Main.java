50b897531ebf1398f56f22301f4771f3d3f0c6a5
AndreySarafankin <andreisarafankin@mail.ru>
2026-03-21 01:47:22 +0300

    reworked commit

--- src/Main.java
+++ src/Main.java
@@ -17,15 +17,15 @@ public class Main {
             System.out.println(x);
         }
         //Задание5
-        for (int e = 1904; e< 2096; e = e +4){
+        for (int e = 1904; e < 2096; e = e + 4) {
             System.out.println(e + " год является високосным");
         }
         //Задание6
-        for (int c = 7; c <=98; c= c +7){
+        for (int c = 7; c <= 98; c = c + 7) {
             System.out.println(c);
         }
         //Задание7
-        for (int r = 1; r <= 512; r = r * 2){
+        for (int r = 1; r <= 512; r = r * 2) {
             System.out.println(r);
         }
         //Задание8
@@ -37,15 +37,15 @@ public class Main {
         }
         //Задание9
         int money1 = 29000;
-        int total1= 0;
+        int total1 = 0;
         for (int w = 1; w <= 12; w++) {
-            total1 = total1 + total1/100;
+            total1 = total1 + total1 / 100;
             total1 = total1 + money1;
             System.out.println("Месяц " + w + ", сумма накоплений равна " + total1 + " рублей");
         }
         //Задание10
-        for (int g = 1; g<=10; g++) {
-            System.out.println("2 * " + g + " = "+(2 * g));
+        for (int g = 1; g <= 10; g++) {
+            System.out.println("2 * " + g + " = " + (2 * g));
         }
     }
 }
